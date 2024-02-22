03f8c8a907fca53bba1a02d0a267847f7389af13
Borys Ushakov <inlvert@gmail.com>
2024-02-22 16:31:22 +0200

    add routing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { carValidation } = require("./middlewares/validation.mw");
-const CarController = require("./controllers/car.controller");
-const { Car } = require("./models");
+// const router = express.Router();
+// const { carValidation } = require("./middlewares/validation.mw");
+// const CarController = require("./controllers/car.controller");
+const router = require("./routers")
 
 // const router = require('./routers/index');
 const app = express();
@@ -13,16 +14,4 @@ app.listen(PORT, () => {
 
 app.use(express.json());
 
-// app.use(router);
-
-app.post("/cars", carValidation, CarController.createCars);
-
-app.get("/cars", CarController.getCars);
-
-app.get("/car", CarController.getCarQuery);
-
-app.get("/cars/:carId", CarController.getCar);
-
-app.delete("/cars/:carId", CarController.deleteCar);
-
-app.put("/cars/:carId", CarController.updateCar);
+app.use(router);
--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { carValidation } = require("../middlewares/validation.mw");
 const CarController = require("../controllers/car.controller");
 
-router.route("/cars").post(CarController.createCars);
+router
+  .route("/cars")
+  .post(carValidation, CarController.createCars)
+  .get(CarController.getCars);
+
+router
+  .route("/cars/:carId")
+  .get(CarController.getCar)
+  .delete(CarController.deleteCar)
+  .put(CarController.updateCar);
+
+router.get("/car", CarController.getCarQuery);
 
 module.exports = router;
